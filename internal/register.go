1dc8f8139e93663fae52a908886ab7e16020fc34
Jonathan Vuillemin <ekkinox@gmail.com>
2024-05-08 16:07:58 +0200

    feat(main): Added SQL probe

--- internal/register.go
+++ internal/register.go
@@ -1,10 +1,14 @@
 package internal
 
 import (
+	"github.com/ankorstore/yokai/fxhealthcheck"
+	"github.com/ankorstore/yokai/sql/healthcheck"
 	"go.uber.org/fx"
 )
 
 // Register is used to register the application dependencies.
 func Register() fx.Option {
-	return fx.Options()
+	return fx.Options(
+		fxhealthcheck.AsCheckerProbe(healthcheck.NewSQLProbe),
+	)
 }
